bc61cdf2e05d309aafa2755ba2e35db257c867e5
Ademola Owoeye <ademolaowoeye07@gmail.com>
2023-10-31 08:36:12 +0100

    queried data

--- queries.sql
+++ queries.sql
@@ -219,3 +219,7 @@ ORDER BY
     visit_count DESC
 LIMIT
     1;
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits WHERE animals_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits WHERE vets_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners WHERE email = 'owner_18327@mail.com';
